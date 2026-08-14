bc39d252f1eb9d05a3791f912170f737beecabde
barranoelia87-a11y <barranoelia87@gmail.com>
2026-08-14 16:22:15 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,13 +1,15 @@
 async function registrar(email, password) {
-  const { data, error } = await supabase.auth.signUp({ email, password });
-  return { data, error };
+    const { data, error } = await supabase.auth.signUp({ email, password });
+    return { data, error };
 }
 
-async function iniciarSesion(email, password) {
-  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
-  return { data, error };
+async function iniciarSession(email, password) {
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+    return { data, error };
 }
 
 supabase.auth.onAuthStateChange((event, session) => {
-  if (!session) window.location.href = "login.html";
+    if (!session) {
+        window.location.href = "login.html";
+    }
 });
